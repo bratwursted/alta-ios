7792a53d2ac28f2c746862011af393c0c597d3dc
Joe Bramhall <joe@thinxmedia.com>
2020-01-12 13:00:47 -0500

    Added view for films list

---
+++ StarWarsReader/FilmsList/FilmsListView.swift
@@ -0,0 +1,22 @@
+//
+//  FilmsListView.swift
+//  StarWarsReader
+//
+//  Created by Joe Bramhall on 1/12/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilmsListView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+// swiftlint:disable type_name
+struct FilmsListView_Previews: PreviewProvider {
+  static var previews: some View {
+    FilmsListView()
+  }
+}
---
+++ StarWarsReader/FilmsList/FilmsListViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  FilmsListViewModel.swift
+//  StarWarsReader
+//
+//  Created by Joe Bramhall on 1/12/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class FilmsListViewModel: ObservableObject {
+
+  private let dataService: Swapi
+
+  private var disposables = Set<AnyCancellable>()
+
+  @Published var films: [FilmsResponse.Film] = []
+
+  init(dataService: Swapi) {
+    self.dataService = dataService
+  }
+
+  func loadFilmsList() {
+    dataService.allFilms()
+      .receive(on: DispatchQueue.main)
+      .sink(receiveCompletion: { completion in
+        switch completion {
+        case .failure(let error):
+          self.films = []
+          print("There was an error getting the films list \(error)")
+        case .finished:
+          break
+        }
+      }, receiveValue: { films in
+          self.films = films
+      })
+  .store(in: &disposables)
+  }
+}
