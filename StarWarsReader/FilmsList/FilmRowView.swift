2467f472d4067da1680c2f37f4907ed0fb205fde
Joe Bramhall <joe@thinxmedia.com>
2020-01-12 13:42:28 -0500

    Film row view and view model

---
+++ StarWarsReader/FilmsList/FilmRowView.swift
@@ -0,0 +1,35 @@
+//
+//  FilmRowView.swift
+//  StarWarsReader
+//
+//  Created by Joe Bramhall on 1/12/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilmRowView: View {
+
+  let viewModel: FilmRowViewModel
+
+  var body: some View {
+    VStack(alignment: .leading, spacing: 6) {
+      Text("Episode \(viewModel.episode) (\(viewModel.released))")
+        .font(.body)
+      Text(viewModel.title)
+        .font(.largeTitle)
+    }
+  }
+}
+
+// swiftlint:disable type_name identifier_name
+struct FilmRowView_Previews: PreviewProvider {
+  static let vm: FilmRowViewModel = {
+    let film = sampleFilms[3]
+    return FilmRowViewModel(film: film)
+  }()
+
+  static var previews: some View {
+    FilmRowView(viewModel: vm)
+  }
+}
---
+++ StarWarsReader/FilmsList/FilmRowViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  FilmRowViewModel.swift
+//  StarWarsReader
+//
+//  Created by Joe Bramhall on 1/12/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import Foundation
+
+struct FilmRowViewModel {
+
+  private let film: FilmsResponse.Film
+
+  init(film: FilmsResponse.Film) {
+    self.film = film
+  }
+
+  var title: String {
+    film.title
+  }
+
+  var episode: String {
+    String(film.episode)
+  }
+
+  var released: String {
+    let calendar = Calendar.current
+    let releaseYear = calendar.component(.year, from: film.releaseDate)
+    return String(releaseYear)
+  }
+
+}
