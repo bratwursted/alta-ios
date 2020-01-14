58a40cf28e391956e8b38420cdef76a5bebd6e31
Joe Bramhall <joe@thinxmedia.com>
2020-01-14 15:09:57 -0500

    Added starship view

---
+++ StarWarsReader/StarshipView/StarshipView.swift
@@ -0,0 +1,21 @@
+//
+//  StarshipView.swift
+//  StarWarsReader
+//
+//  Created by Joe Bramhall on 1/14/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct StarshipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StarshipView_Previews: PreviewProvider {
+    static var previews: some View {
+        StarshipView()
+    }
+}
---
+++ StarWarsReader/StarshipView/StarshipViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  StarshipViewModel.swift
+//  StarWarsReader
+//
+//  Created by Joe Bramhall on 1/14/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import Foundation
