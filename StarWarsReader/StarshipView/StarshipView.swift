3dfacc11fadc54647ba73a9d13bd7e51e6d286a2
Joe Bramhall <joe@thinxmedia.com>
2020-01-14 16:33:34 -0500

    Basic view details

--- StarWarsReader/StarshipView/StarshipView.swift
+++ StarWarsReader/StarshipView/StarshipView.swift
@@ -9,14 +9,76 @@
 import SwiftUI
 
 struct StarshipView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+  @ObservedObject var viewModel: StarshipViewModel
+
+  var body: some View {
+    List {
+      if viewModel.starship == nil {
+        Text("No result")
+      } else {
+        primarySection
+        speedAndPowerSection
+        crewCapacitySection
+      }
     }
+    .navigationBarTitle(Text(viewModel.viewTitle), displayMode: .inline)
+    .onAppear {
+      self.viewModel.loadStarship()
+    }
+  }
+}
+
+extension StarshipView {
+
+  var primarySection: some View {
+    Section {
+      VStack(alignment: .leading, spacing: 12) {
+        Text(verbatim: viewModel.name)
+        Text(verbatim: "Manufacturer: \(viewModel.manufacturer)")
+        Text(verbatim: "Cost: \(viewModel.cost)")
+      }
+    }
+  }
+
+  var crewCapacitySection: some View {
+    Section(header: Text("Crew & Capacity")) {
+      VStack(alignment: .leading, spacing: 12) {
+        Text(verbatim: "Class: \(viewModel.classification)")
+        Text(verbatim: "Length: \(viewModel.length)")
+        Text(verbatim: "Crew: \(viewModel.crew)")
+        Text(verbatim: "Passengers: \(viewModel.passengers)")
+        Text(verbatim: "Cargo: \(viewModel.cargo)")
+        Text(verbatim: "Consumables: \(viewModel.consumables)")
+      }
+    }
+  }
+
+  var speedAndPowerSection: some View {
+    Section(header: Text("Speed & Power")) {
+      VStack(alignment: .leading, spacing: 12) {
+        Text(verbatim: "Max. air speed: \(viewModel.maximumSpeed)")
+        Text(verbatim: "Megalights: \(viewModel.megalights)")
+        Text(verbatim: "Hyperdrive rating: \(viewModel.hyperdriveClass)")
+      }
+    }
+  }
 }
 
 // swiftlint:disable all
 struct StarshipView_Previews: PreviewProvider {
-    static var previews: some View {
-        StarshipView()
+  static let vm: StarshipViewModel = {
+    let falcon = loadSampleStarship("falcon")
+    let service = MockDataService(falcon)
+    return StarshipViewModel(
+      resourceId: falcon.starshipId,
+      dataService: service
+    )
+  }()
+
+  static var previews: some View {
+    NavigationView {
+      StarshipView(viewModel: vm)
     }
+  }
 }
