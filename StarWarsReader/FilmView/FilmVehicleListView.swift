fae936d8011493a1f037809ba256490f26152b8a
Joe Bramhall <joe@thinxmedia.com>
2020-01-13 22:04:20 -0500

    Added vehicle list

---
+++ StarWarsReader/FilmView/FilmVehicleListView.swift
@@ -0,0 +1,22 @@
+//
+//  FilmVehicleListView.swift
+//  StarWarsReader
+//
+//  Created by Joe Bramhall on 1/13/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilmVehicleListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// swiftlint:disable all
+struct FilmVehicleListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilmVehicleListView()
+    }
+}
---
+++ StarWarsReader/FilmView/FilmVehicleRowView.swift
@@ -0,0 +1,22 @@
+//
+//  FilmVehicleRowView.swift
+//  StarWarsReader
+//
+//  Created by Joe Bramhall on 1/13/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilmVehicleRowView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+// swiftlint:disable all
+struct FilmVehicleRowView_Previews: PreviewProvider {
+  static var previews: some View {
+    FilmVehicleRowView()
+  }
+}
